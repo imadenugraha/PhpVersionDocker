5273537c8e7c6478a64a3005dce6c4dfa30a5c94
I Made N. J. P <imadenugraha@gmail.com>
2022-07-02 00:03:02 +0700

    First Commit

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Version Docker</title>
+</head>
+<body>
+    <center><h2>PHP Version using Docker</h2></center>
+    <?php
+        phpinfo(INFO_ALL);
+    ?>
+</body>
+</html>
